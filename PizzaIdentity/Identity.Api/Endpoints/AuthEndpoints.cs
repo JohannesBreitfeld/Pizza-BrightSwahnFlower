614f55da2e3aba4ce493639c518602f7ede668e5
Johannes Breitfeld <jbvbreitfeld@gmail.com>
2025-12-08 15:08:43 +0100

    Initial setup

---
+++ PizzaIdentity/Identity.Api/Endpoints/AuthEndpoints.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Identity.Core.Features;
+using MediatR;
+
+namespace Identity.Api.Endpoints;
+
+public static class AuthEndpoints
+{
+    public static IEndpointRouteBuilder AddAuthEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("api/auth/login", HandleLoginAsync)
+            .WithName("Login")
+            .WithTags("Auth")
+            .ProducesValidationProblem();
+
+        return app;
+    }
+
+    private static async Task<IResult> HandleLoginAsync(
+        LoginCommand command,
+        IMediator mediator,
+        IValidator<LoginCommand> validator,
+        CancellationToken cancellationToken)
+    {
+        var validationResult = await validator.ValidateAsync(command, cancellationToken);
+
+        if (!validationResult.IsValid)
+            return Results.ValidationProblem(validationResult.ToDictionary());
+
+        var response = await mediator.Send(command, cancellationToken);
+
+        return Results.Ok(response);
+    }
+}
---
+++ PizzaIdentity/Identity.Core/Contracts/IUserCommandRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Identity.Core.Contracts;
+
+public interface IUserCommandRepository
+{
+    Task<
+}
