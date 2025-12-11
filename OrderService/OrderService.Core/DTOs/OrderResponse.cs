9527a0201bf8189e4e2cba5b889b621cc7f81f00
Johannes Breitfeld <jbvbreitfeld@gmail.com>
2025-12-11 13:08:49 +0100

    Add Endpoint for getting all orders and seed 5 orders at startup

---
+++ OrderService/OrderService.Core/DTOs/OrderResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderService.Core.DTOs
+{
+    public sealed record OrderResponse
+    (
+        string Id,
+        string CustomerEmail,
+        IEnumerable<OrderItemResponse> Items,
+        decimal TotalAmount,
+        decimal TaxAmount,
+        DateTime CreatedAt
+    );
+}
---
+++ OrderService/OrderService.Core/Features/GetAllOrders/GetAllOrdersQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace OrderService.Core.Features.GetAllOrders;
+
+public sealed record GetAllOrdersQuery : IRequest<GetAllOrdersResponse>
+{
+    public GetAllOrdersQuery(int? page, int? pageSize)
+    {
+        Page = page;
+        PageSize = pageSize;
+    }
+
+    public int? Page { get; init; } = null;
+    public int? PageSize { get; init; } = null;
+
+}
