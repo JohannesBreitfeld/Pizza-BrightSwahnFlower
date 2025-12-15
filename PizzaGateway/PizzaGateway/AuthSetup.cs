b131384bc4f5151927a2e1b2bc8ac006e33ba8dc
Johannes Breitfeld <Johannes.Breitfeld@iths.se>
2025-12-15 08:15:11 +0000

    Merged PR 25: #78 Add auth to gatway
    #78
    
    Related work items: #78

---
+++ PizzaGateway/PizzaGateway/AuthSetup.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PizzaGateway;
+
+public static class AuthSetup
+{
+    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtSettings = configuration.GetSection("JwtSettings") ?? throw new InvalidOperationException("JWT Settings is not configured"); ;
+        var secretKey = jwtSettings["Secret"] ?? throw new InvalidOperationException("JWT Secret is not configured");
+        var issuer = jwtSettings["Issuer"] ?? throw new InvalidOperationException("JWT Issuer is not configured");
+        var audience = jwtSettings["Audience"] ?? throw new InvalidOperationException("JWT Audience is not configured");
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer("Bearer", options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = issuer,
+                ValidAudience = audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+                ClockSkew = TimeSpan.Zero,
+                RoleClaimType = ClaimTypes.Role,
+                NameClaimType = ClaimTypes.NameIdentifier
+            };
+        });
+
+        services.AddAuthorization();
+
+        return services;
+    }
+}
--- PizzaGateway/PizzaGateway/Program.cs
+++ PizzaGateway/PizzaGateway/Program.cs
@@ -1,11 +1,14 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using PizzaGateway;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
     .SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot-configuration/ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +20,9 @@ var app = builder.Build();
 app.UseSwaggerForOcelotUI(opt => {
     opt.PathToSwaggerGenerator = "/swagger/docs";
 });
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 await app.UseOcelot();
 
